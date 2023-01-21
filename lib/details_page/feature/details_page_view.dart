4ec98cff0de42b191f94d38184eaed9bff654b1f
Zakaria02 <zakaria.bensalem.1@gmail.com>
2023-01-21 19:17:15 +0100

    add init details page

---
+++ lib/details_page/feature/details_page_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'components/body.dart';
+
+class BudgetDetailsPageView extends StatelessWidget {
+  const BudgetDetailsPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BudgetDetailsPageBody(),
+    );
+  }
+}
