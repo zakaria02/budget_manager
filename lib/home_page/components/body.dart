e32c2114a5c5430c7ec37151178afa5c16bd98c7
Zakaria02 <zakaria.bensalem.1@gmail.com>
2023-01-21 16:57:39 +0100

    add home page view

--- lib/home_page/components/body.dart
+++ lib/home_page/components/body.dart
@@ -1,3 +1,4 @@
+import 'package:budget_manager/home_page/components/components.dart';
 import 'package:flutter/material.dart';
 
 class HomePageBody extends StatelessWidget {
@@ -5,6 +6,8 @@ class HomePageBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return ListView(
+      children: const [HomePageAppBar()],
+    );
   }
 }
--- lib/home_page/components/components.dart
+++ lib/home_page/components/components.dart
@@ -2,3 +2,4 @@ export 'app_bar.dart';
 export 'date_picker.dart';
 export 'body.dart';
 export 'fab.dart';
+export 'bottom_bar.dart';
