e90de3e364a403dc3f8c833f3ba99379f50f3feb
Zakaria02 <zakaria.bensalem.1@gmail.com>
2023-01-21 17:16:27 +0100

    add amount item component

--- lib/styles/texts.dart
+++ lib/styles/texts.dart
@@ -1,31 +1,19 @@
 import 'package:flutter/material.dart';
 
-/// All texts Styles must be declared here
-const TextStyle h1 = TextStyle(
-  fontFamily: "Quicksand",
-  fontSize: 32,
-  fontWeight: FontWeight.w600,
-  color: Colors.black,
-  letterSpacing: 2,
-);
+import 'colors.dart';
 
-const TextStyle h3 = TextStyle(
-  fontFamily: "Quicksand",
-  fontSize: 18,
-  fontWeight: FontWeight.w500,
-  color: Colors.black,
-);
+/// All texts Styles must be declared here
 
 const TextStyle body = TextStyle(
   fontFamily: "Poppins",
   fontSize: 15,
-  color: Colors.black,
+  color: black,
 );
 
 const TextStyle pageTitle = TextStyle(
   fontFamily: "Quicksand",
   fontSize: 17,
-  color: Colors.black,
+  color: black,
   fontWeight: FontWeight.w600,
   letterSpacing: 2,
 );
@@ -37,15 +25,12 @@ TextStyle appBarButton = TextStyle(
   fontWeight: FontWeight.w600,
 );
 
-const TextStyle textButton = TextStyle(
+const TextStyle h2 = TextStyle(
   fontFamily: "Quicksand",
   fontSize: 14,
-  color: Colors.black,
-  letterSpacing: 2,
+  color: black,
+  fontWeight: FontWeight.w500,
 );
 
-const TextStyle buttonBody = TextStyle(
-  color: Colors.white,
-  fontSize: 14,
-  fontWeight: FontWeight.w600,
-);
+const TextStyle value =
+    TextStyle(fontFamily: "Poppins", fontSize: 18, color: black);
