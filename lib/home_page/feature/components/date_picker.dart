ce2290d612e7f3973eeef5e8276bf02cf42bf9db
Zakaria02 <zakaria.bensalem.1@gmail.com>
2023-01-21 18:02:11 +0100

    add bloc to date picker component

--- lib/home_page/feature/components/date_picker.dart
+++ lib/home_page/feature/components/date_picker.dart
@@ -1,8 +1,12 @@
 import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:unicons/unicons.dart';
 
 import '../../../styles/styles.dart';
+import '../../../utils/constantes.dart';
+import '../bloc/date_picker/date_picker_cubit.dart';
+import '../../../extensions/date_extension.dart';
 
 class DatePicker extends StatelessWidget {
   const DatePicker({super.key});
@@ -11,8 +15,8 @@ class DatePicker extends StatelessWidget {
     final DateTime? picked = await showDatePicker(
       context: context,
       initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2050),
+      firstDate: minDate,
+      lastDate: maxDate,
       fieldLabelText: "Pick Date",
       fieldHintText: 'Month/Date/Year',
     );
@@ -23,37 +27,51 @@ class DatePicker extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        IconButton(
-          onPressed: () {},
-          icon: const Icon(
-            UniconsLine.angle_left_b,
-          ),
-          color: black,
-        ),
-        TextButton(
-          onPressed: () => _showDatePciker(
-            context,
-          ),
-          style: TextButton.styleFrom(
-            surfaceTintColor: Colors.grey,
-          ),
-          child: const Text(
-            "",
-            style: body,
-          ),
-        ),
-        IconButton(
-          onPressed: () {},
-          icon: const Icon(
-            UniconsLine.angle_right_b,
-          ),
-          color: black,
-        ),
-      ],
+    return BlocBuilder<DatePickerCubit, DatePickerState>(
+      builder: (context, datePickerState) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              onPressed: datePickerState.budgetDate.difference(minDate).inDays >
+                      30
+                  ? () {
+                      BlocProvider.of<DatePickerCubit>(context).subtractMonth();
+                    }
+                  : null,
+              icon: const Icon(
+                UniconsLine.angle_left_b,
+              ),
+              color: black,
+            ),
+            TextButton(
+              onPressed: () => _showDatePciker(
+                context,
+              ),
+              style: TextButton.styleFrom(
+                surfaceTintColor: Colors.grey,
+              ),
+              child: Text(
+                datePickerState.budgetDate.stringDateTime(),
+                style: body,
+              ),
+            ),
+            IconButton(
+              onPressed:
+                  maxDate.difference(datePickerState.budgetDate).inDays > 30
+                      ? () {
+                          BlocProvider.of<DatePickerCubit>(context).addMonth();
+                        }
+                      : null,
+              icon: const Icon(
+                UniconsLine.angle_right_b,
+              ),
+              color: black,
+            ),
+          ],
+        );
+      },
     );
   }
 }
