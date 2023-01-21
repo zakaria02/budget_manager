a3b1d37bc10286643378102a684865c65e9733c8
Zakaria02 <zakaria.bensalem.1@gmail.com>
2023-01-21 16:45:34 +0100

    add date picker component

---
+++ lib/home_page/components/date_picker.dart
@@ -0,0 +1,59 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'package:unicons/unicons.dart';
+
+import '../../styles/styles.dart';
+
+class DatePicker extends StatelessWidget {
+  const DatePicker({super.key});
+
+  void _showDatePciker(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2050),
+      fieldLabelText: "Pick Date",
+      fieldHintText: 'Month/Date/Year',
+    );
+    if (picked != null) {
+      log(picked.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            UniconsLine.angle_left_b,
+          ),
+          color: black,
+        ),
+        TextButton(
+          onPressed: () => _showDatePciker(
+            context,
+          ),
+          style: TextButton.styleFrom(
+            surfaceTintColor: Colors.grey,
+          ),
+          child: const Text(
+            "",
+            style: body,
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            UniconsLine.angle_right_b,
+          ),
+          color: black,
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/styles/texts.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+/// All texts Styles must be declared here
+const TextStyle h1 = TextStyle(
+  fontFamily: "Quicksand",
+  fontSize: 32,
+  fontWeight: FontWeight.w600,
+  color: Colors.black,
+  letterSpacing: 2,
+);
+
+const TextStyle h3 = TextStyle(
+  fontFamily: "Quicksand",
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+  color: Colors.black,
+);
+
+const TextStyle body = TextStyle(
+  fontFamily: "Poppins",
+  fontSize: 15,
+  color: Colors.black,
+);
+
+const TextStyle pageTitle = TextStyle(
+  fontFamily: "Quicksand",
+  fontSize: 17,
+  color: Colors.black,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 2,
+);
+
+TextStyle appBarButton = TextStyle(
+  fontFamily: "Quicksand",
+  fontSize: 16,
+  color: Colors.grey[700],
+  fontWeight: FontWeight.w600,
+);
+
+const TextStyle textButton = TextStyle(
+  fontFamily: "Quicksand",
+  fontSize: 14,
+  color: Colors.black,
+  letterSpacing: 2,
+);
+
+const TextStyle buttonBody = TextStyle(
+  color: Colors.white,
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+);
