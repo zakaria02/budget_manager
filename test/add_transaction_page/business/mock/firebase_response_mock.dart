827826dd6b6be7ddc56a691066bee728de225a64
zakaria02 <zakaria.bensalem.1@gmail.com>
2023-02-09 20:23:11 +0100

    remove tests

--- test/add_transaction_page/business/mock/firebase_response_mock.dart
+++
@@ -1,7 +0,0 @@
-import 'package:budget_manager/utils/model/firebase_response.dart';
-
-FirebaseResponse successResponse =
-    FirebaseResponse(success: true, message: "Transaction added succefully");
-
-FirebaseResponse errorResponse =
-    FirebaseResponse(success: false, message: "Error while adding transaction");
